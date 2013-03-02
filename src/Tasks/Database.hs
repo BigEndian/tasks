8a16005222a4a4912c128a5358ad1f42b81020fb
Eric Carr <superjapanfreak@gmail.com>
2013-03-02 08:52:54 -0800

    in flux

--- src/Tasks/Database.hs
+++ src/Tasks/Database.hs
@@ -2,30 +2,15 @@ module Tasks.Database(Database) where
 
 
 import Tasks.Task
-import Tasks.TaskList(TaskList)
+import Tasks.TaskList
 
 import System.IO
 import Data.Maybe(Maybe(..))
-import Text.ParserCombinators.Parsec
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as B
+import Data.Word(Word8(..))
+import Data.Binary
 
 data Database = Database { databaseFileName :: FilePath, databaseTaskList :: IO TaskList }
 
-{-
-readTask :: Handle -> IO Task
-readTask h = do
-               line <- hGetLine h
-               return $ task line Nothing
+exHandle = openFile "/home/eric/.tasks" ReadMode
 
-exHandle = openFile "/home/eric/.tasks" ReadMode -}
-
-databaseFile :: GenParser Task st TaskList
-databaseFile =
-   do result <- many taskLine
-      eof
-      return result
-
-taskLine :: GenParser Char st Task
-taskLine =
-   do name  <- taskInfoName
-      return $ task name Nothing
