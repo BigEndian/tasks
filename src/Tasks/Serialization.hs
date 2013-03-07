e318e7d3cdb43e2e09158fe967a78b838f351dbb
Eric Carr <superjapanfreak@gmail.com>
2013-03-07 15:17:54 -0800

    Removed a lot of no-longer-necessary code

--- src/Tasks/Serialization.hs
+++ src/Tasks/Serialization.hs
@@ -1,14 +1,12 @@
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+
 module Tasks.Serialization(
    convertWord8ToChar,
    convertCharToWord8,
    
    stringToWByteString,
    wByteStringToCByteString,
-   cByteStringToWByteString,
-
-   putString,
-   putWByteString,
-   putCByteString)
+   cByteStringToWByteString)
    where
 
 {-
@@ -29,7 +27,6 @@ convertWord8ToChar = chr . fromIntegral
 convertCharToWord8 :: Char -> Word8
 convertCharToWord8 = fromIntegral . ord
 
-
 stringToWByteString :: String -> BW.ByteString
 stringToWByteString = BW.pack . map convertCharToWord8
 
@@ -38,31 +35,9 @@ wByteStringToString = map convertWord8ToChar . BW.unpack
 
 wByteStringToCByteString :: BW.ByteString -> BC.ByteString
 wByteStringToCByteString =
-   BC.pack . (map convertWord8ToChar) . BW.unpack
+   BC.pack . wByteStringToString
 
 cByteStringToWByteString :: BC.ByteString -> BW.ByteString
 cByteStringToWByteString =
    BW.pack . (map convertCharToWord8) . BC.unpack
 
-putString :: String -> Put
-putString [] = 
-   fail "Invalid string passed to putString! String must be one character or longer!"
-putString str =
-   case length str of
-      1 -> putWord8 w
-      _ -> do putWord8 w; putString (drop 1 str)
-   where
-      c = let (fc:_) = (take 1 str) in fc
-      w = convertCharToWord8 c
-
-putWByteString :: BW.ByteString -> Put
-putWByteString wbs =
-   case BW.length wbs of
-      0 -> fail "Invalid string passed to putByteString! String must be one character or longer!"
-      1 -> putWord8 w
-      _ -> do putWord8 w; putWByteString (BW.drop 1 wbs)
-   where
-      w = let (fw:_) = BW.unpack $ BW.take 1 wbs in fw
-
-putCByteString :: BC.ByteString -> Put
-putCByteString cbs = putWByteString (cByteStringToWByteString cbs)
