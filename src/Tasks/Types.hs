57ca8735c563ad98a6f4c068af19ea2c3cc9ac75
Eric Carr <superjapanfreak@gmail.com>
2013-03-07 17:40:03 -0800

    Reformatted the file, added haddock documentation

--- src/Tasks/Types.hs
+++ src/Tasks/Types.hs
@@ -1,25 +1,21 @@
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+-- |
+-- Module: Tasks.Types
+--
+-- This module contains types relevant to the Tasks module,
+-- but currently only exports TaskString and related functions,
+-- useful for custom implementations of Binary.
 
-module Tasks.Serialization(
-   convertWord8ToChar,
-   convertCharToWord8,
-   
-   stringToWByteString,
-   wByteStringToCByteString,
-   cByteStringToWByteString)
-   where
-
-{-
- - Basic methods to help me parse and store Tasks
- -}
+module Tasks.Types
+   (
+     TaskString
+   , tsString
+   , tsToString
+   , tsEmpty
+   ) where
 
 import qualified Data.ByteString as BW
-import qualified Data.ByteString.Char8 as BC
-import Data.Maybe
-import Data.Binary -- for put
-import Data.Word(Word8(..))
+import Data.Binary
 import Data.Char(chr, ord)
-import Data.Bits
 
 convertWord8ToChar :: Word8 -> Char
 convertWord8ToChar = chr . fromIntegral
@@ -33,11 +29,35 @@ stringToWByteString = BW.pack . map convertCharToWord8
 wByteStringToString :: BW.ByteString -> String
 wByteStringToString = map convertWord8ToChar . BW.unpack
 
-wByteStringToCByteString :: BW.ByteString -> BC.ByteString
-wByteStringToCByteString =
-   BC.pack . wByteStringToString
+-- | A custom type wrapping around Data.ByteString in order
+-- to implement a custom instance of the Binary typeclass
+newtype TaskString = TaskString BW.ByteString deriving (Read, Show, Eq)
 
-cByteStringToWByteString :: BC.ByteString -> BW.ByteString
-cByteStringToWByteString =
-   BW.pack . (map convertCharToWord8) . BC.unpack
+-- | Create a TaskString given a String
+tsString :: String -> TaskString
+tsString = TaskString . stringToWByteString
 
+-- | Convert a TaskString to a String
+tsToString :: TaskString -> String
+tsToString (TaskString bws) = wByteStringToString bws
+
+-- | Check whether a given TaskString is empty
+tsEmpty :: TaskString -> Bool
+tsEmpty (TaskString bws) = bws == BW.empty
+
+word8sToTaskString :: [Word8] -> TaskString
+word8sToTaskString = TaskString . BW.pack
+
+instance Binary TaskString where
+   get = do
+            (return . word8sToTaskString . init) =<< readWord8sUntil 0
+         where
+            readWord8sUntil :: Word8 -> Get [Word8]
+            readWord8sUntil val = do
+               w8 <- getWord8
+               if w8 == val then
+                  return $ [w8]
+               else
+                  (return . (w8:)) =<< (readWord8sUntil val)
+
+   put (TaskString bws) = mapM_ putWord8 $ (BW.unpack bws) ++ [0]
